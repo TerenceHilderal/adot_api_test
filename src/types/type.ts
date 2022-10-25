09457840e8cb7a7b84f078e774a3900281872a7d
Térence Hildéral <56540121+TerenceHilderal@users.noreply.github.com>
2022-10-25 21:13:31 +0200

    :heavy_plus_sign: add types

---
+++ src/types/type.ts
@@ -0,0 +1,29 @@
+type Payload = Array<Data>
+
+type Data = {
+lat : number,
+lon : number,
+name: string,
+}
+
+type ApiResponse = {
+lat : number,
+lon : number,
+name: string,
+impressions:number,
+clicks:number
+}
+
+type City = string
+
+
+
+
+
+
+
+
+
+
+
+
